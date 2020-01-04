9047ff24c5d6dbc80d875411445d59ce562955aa
Mithwindy <khuangustc@gmail.com>
2020-01-05 02:55:31 +0800

    k8s

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,11 +8,12 @@
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacityp4k8sv2 --image=caskeep/udacityp4:second
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment/udacityp4k8sv2 --type="NodePort" --port 80
